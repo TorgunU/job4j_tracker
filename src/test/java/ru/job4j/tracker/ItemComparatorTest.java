4149f6f52ab3453409c9d8fa5e0f35aa83042a46
ITorgun <leoffanb@gmail.com>
2024-01-13 16:01:26 +0300

    0. Сортировка [#10096 #473127]

---
+++ src/test/java/ru/job4j/tracker/ItemComparatorTest.java
@@ -0,0 +1,49 @@
+package ru.job4j.tracker;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ItemComparatorTest {
+    @Test
+    void whenAscendingSort() {
+        List<Item> itemList = new LinkedList<>(Arrays.asList(
+                new Item("DoSomething"),
+                new Item("Work"),
+                new Item("Sleep"),
+                new Item("Awake")
+        ));
+        List<Item> expected = new LinkedList<>(Arrays.asList(
+                new Item("Awake"),
+                new Item("DoSomething"),
+                new Item("Sleep"),
+                new Item("Work")
+        ));
+
+        itemList.sort(new ItemAscByName());
+        assertThat(itemList).isEqualTo(expected);
+    }
+
+    @Test
+    void whenDescendingSort() {
+        List<Item> itemList = new LinkedList<>(Arrays.asList(
+                new Item("Sleep"),
+                new Item("DoSomething"),
+                new Item("Work"),
+                new Item("Awake")
+        ));
+        List<Item> expected = new LinkedList<>(Arrays.asList(
+                new Item("Work"),
+                new Item("Sleep"),
+                new Item("DoSomething"),
+                new Item("Awake")
+        ));
+
+        itemList.sort(new ItemDescByName());
+        assertThat(itemList).isEqualTo(expected);
+    }
+}
